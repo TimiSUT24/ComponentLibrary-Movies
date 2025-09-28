e5b0a85807d862475d828dfb8b1a3ada37e41553
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2025-09-28 18:49:29 +0200

    Add moviedetails logic

---
+++ ComponentLibrary/src/Components/MovieDetails.jsx
@@ -0,0 +1,31 @@
+import {useEffect, useState} from 'react'
+
+function MovieDetails ({imdbID, onClose}) {
+    const [details, setDetails] = useState(null)
+    const apiKey = import.meta.env.VITE_API_KEY
+
+    useEffect (() => {
+        fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${imdbID}&plot=full`)
+        .then(res => res.json())
+        .then(data => setDetails(data))
+    }, [imdbID, apiKey]) 
+
+    if(!details){
+        return <p>Loading...</p>
+    }
+
+    return(
+        <div className ="movie-details">
+            <Link to="/">Back</Link>
+            <h1>{details.Title} ({details.Year})</h1>
+            <img src={details.Poster} alt={details.Title} />
+            <p><strong>Director:</strong> {details.Director}</p>
+            <p><strong>Actors:</strong> {details.Actors}</p>
+            <p><strong>Plot:</strong> {details.Plot}</p>
+        </div>
+    )
+
+
+}
+
+export default MovieDetails;
