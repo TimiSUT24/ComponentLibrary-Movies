3651e3c064525ad219c87d08dc2117c0d6f79ea4
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2025-10-04 14:57:55 +0200

    Added TutorialView page that shows how components work

--- ComponentLibrary/src/Pages/TutorialView.jsx
+++ ComponentLibrary/src/Pages/TutorialView.jsx
@@ -3,6 +3,7 @@ import SearchInput from '../Components/Search';
 import MovieList from '../Components/MovieList';
 import Favorites from '../Components/FavoriteList';
 import MovieDetails from "../Components/MovieDetails";
+import '../CSS/TutorialView.css';
 
 
 function Tutorial () {
@@ -27,62 +28,114 @@ const toggleFavorite = (movie) => {
     return(
         <div className = "tutorial-view">
             <h1><strong>Component Library Tutorial</strong></h1>
-            <p>Here you can see examples of how the components work</p>
-            <ul>
-                <li>
-                <p><strong>----SearchBar----</strong></p>
+            <p style={{fontSize: '17px'}}>Here you can see examples of how the components work</p>
+
+            <ul className ="tutorials">
+                <li className ="searchbar-tutorial">
+                <p style={{fontSize:'20px'}}><strong>SearchBar</strong></p>
                 <p>Search for movies with title</p>
-                <p>How to use {'<SearchInput onSearch = {searchMovies}></SearchInput>'}</p>
-                <p>Visual presentation</p>
-                <SearchInput onSearch = {handleSearch}></SearchInput>
+                <p className="yellow-text">{'</>'} Code example </p>
+                <div className ="code-example">                   
+                    <p> {`
+                    import SearchInput from '../Components/Search';
+
+                    const handleSearch = (query) =>
+                    { alert("Searching for:" + query) }
+
+                        <SearchInput onSearch = {handleSearch}></SearchInput>`}                                                                                   
+                    </p>
+                </div>
+                <p className="yellow-text">Live Demo</p>
+                <div className ="presentation">
+                    <SearchInput onSearch = {handleSearch}></SearchInput>
+                </div>
             </li>
 
-            <li>
-                <p><strong>----MovieCard/MovieList----</strong></p>
+            <li className ="movies-tutorial">
+                <p style={{fontSize:'20px'}}><strong>MovieCard/MovieList</strong></p>
                 <p>MovieCard shows Image,Picture,Title,Year and MovieList shows a list of this MovieCards so they are multiple</p>
-                <p>How to use (MovieCard) {'<MovieCard key = {m.imdbID} movie = {m} isFavorite = {favorites.some(f => f.imdbID === m.imdbID)} toggleFavorite = {toggleFavorite} </MovieCard>'}</p>
-                <p>(MovieList) {'<MovieList movies = {movies}  favorites = {favorites}  toggleFavorite = {toggleFavorite}> </MovieList>'}</p>
-                <p>Visual presentation</p>
+                <p className="yellow-text">{'</>'} Code example </p>
+                <div className ="code-example">                   
+                    <p> {`
+                    (MovieCard) 
+                    import MovieCard from "./MovieCard";
+
+                    <MovieCard key = {m.imdbID} movie = {m} isFavorite = {favorites.some(f => f.imdbID === m.imdbID)} toggleFavorite = {toggleFavorite} </MovieCard>
+
+                    (MovieList) 
+                    import MovieList from '../Components/MovieList';
+
+                    <MovieList movies = {movies}  favorites = {favorites}  toggleFavorite = {toggleFavorite}> </MovieList>
+                    `}                                                                                   
+                    </p>
+                </div>      
+                <p className="yellow-text">Live Demo</p>
+                <div className="presentations">
                 <MovieList
                 key={demoMovies.imdbID}
                 movies={demoMovies}
                 isFavorite={false}
                 toggleFavorite={toggleFavorite}
                 />
+                </div>               
             </li>
 
-            <li>
-                <p><strong>----MovieDetails----</strong></p>
+            <li className ="movieDetails-tutorial">
+                <p style={{fontSize:'20px'}}><strong>MovieDetails</strong></p>
                 <p>See more info about the movie like actors,plot,Director</p>
-                <p>How to use {'<Link to={`/movie/${movie.imdbID}`}>Details</Link>'}</p>
-                <p>Visual presentation</p>
-                <MovieDetails imdbID = {demoMovies.imdbID = 'tt6300910'}/>
+                <p className="yellow-text">{'</>'} Code example </p>
+                <div className ="code-example">                   
+                    <p> {`
+                    import MovieDetails from "../Components/MovieDetails";
+
+                    <MovieDetails imdbID ={imdbID}></MovieDetails>
+                    `}                                                                                   
+                    </p>
+                </div> 
+                <p className="yellow-text">Live Demo</p>
+                    <MovieDetails imdbID = {demoMovies.imdbID = 'tt6300910'}/>          
             </li>
 
-            <li>
-        <p><strong>----Favorites----</strong></p>
+            <li className ="favorites-tutorial">
+        <p style={{fontSize:'20px'}}><strong>Favorites</strong></p>
         <p>
             Handles adding/removing movies to a persistent list (localStorage in
             the real app).
         </p>
-        <p>
-            How to use:{" "}
-            <code>
-            {`const [favorites, setFavorites] = useFavorites();`}
-            <br />
-            {`<MovieCard movie={m} isFavorite={favorites.some(...)} toggleFavorite={toggleFavorite} />`}
-            </code>
-        </p>
-        <p>Visual presentation:</p>
-        {favorites.length === 0 ? (
-            <p>No favorites yet. Click favorite button on a MovieCard to add.</p>
-        ) : (
-            <ul>
-            {favorites.map((f) => (
-                <li key={f.imdbID}>{f.Title} ({f.Year})</li>
-            ))}
-            </ul>
-        )}
+        <p className="yellow-text">{'</>'} Code example </p>
+        <div className ="code-example">                   
+                    <p> {`
+                    import Favorites from '../Components/FavoriteList';
+                  
+                    const [favorites, setFavorite] = useState([]);                  
+
+                    <Favorites
+                    favorites = {favorites}
+                    removeFavorite = {id => setFavorite(favorites.filter(f => f.imdbID !== id))}>   
+                    </Favorites>
+                    
+                    `}                                                                                   
+                    </p>
+                </div> 
+                
+        <p className="yellow-text">Live Demo</p>
+        <Favorites
+        favorites = {favorites}
+        removeFavorite = {id => setFavorite(favorites.filter(f => f.imdbID !== id))}>   
+        </Favorites> 
+        <button
+        onClick={() => {
+            const movie = { imdbID: 'tt630091011rrr' };
+
+            const isFavorite = favorites.some(f => f.imdbID === movie.imdbID);
+
+            if (isFavorite) {
+                setFavorite(favorites.filter(f => f.imdbID !== movie.imdbID));
+            } else {
+                setFavorite([...favorites, movie]);
+            }
+        }}
+>Favorite</button>      
         </li>
             </ul>                       
         </div>    
